0f58a002eac715b3642cbba86afa913cc62db987
Mishywayu <michellewayua2@gmail.com>
2024-08-26 15:44:37 +0300

    issue fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <Navbar />
+      </div>
       <h1>Hello</h1>
     </div>
   );
