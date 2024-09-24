51690268578ccc9db83fffd82a7bb95ecfddb304
Mishywayu <michellewayua2@gmail.com>
2024-09-24 09:22:58 +0300

    Navbar and React-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <div>
+    <Router>
+      <div className='relative'>
+        {/* Navbar will be visible in all pages */}
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products/>} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
       </div>
-      <h1>Hello</h1>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Welcome to Get in Touch Page</h1>
+    </div>
+  )
+}
